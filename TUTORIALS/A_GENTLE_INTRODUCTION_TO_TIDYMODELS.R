038b3be02a12cfc881c95d9d2e242f50cb0a7506
Jonathan Kitt <jonathan.kitt@proton.me>
2024-08-21 12:11:48 +0200

    finised tutorial

--- TUTORIALS/A_GENTLE_INTRODUCTION_TO_TIDYMODELS.R
+++ TUTORIALS/A_GENTLE_INTRODUCTION_TO_TIDYMODELS.R
@@ -34,4 +34,61 @@ glimpse(iris_training)
 
 # MODEL TRAINING ----------------------------------------------------------
 
+iris_ranger <- rand_forest(trees = 100, mode = "classification") |> 
+  set_engine("ranger") |> 
+  fit(Species ~ ., data = iris_training)
 
+iris_rf <- rand_forest(trees = 100, mode = "classification") |> 
+  set_engine("randomForest") |> 
+  fit(Species ~ ., data = iris_training)
+
+predict(iris_ranger, iris_testing)
+
+# MODEL VALIDATION --------------------------------------------------------
+
+iris_ranger |> 
+  predict(iris_testing) |> 
+  bind_cols(iris_testing) |> 
+  glimpse()
+
+iris_ranger |> 
+  predict(iris_testing) |> 
+  bind_cols(iris_testing) |> 
+  metrics(truth = Species, estimate = .pred_class)
+
+iris_rf |> 
+  predict(iris_testing) |> 
+  bind_cols(iris_testing) |> 
+  metrics(truth = Species, estimate = .pred_class)
+
+iris_ranger |> 
+  predict(iris_testing, type = "prob") |> 
+  glimpse()
+
+iris_probs <- iris_ranger |> 
+  predict(iris_testing, type = "prob") |> 
+  bind_cols(iris_testing)
+
+glimpse(iris_probs)
+
+iris_probs |> 
+  gain_curve(Species, .pred_setosa:.pred_virginica) |> 
+  glimpse()
+
+iris_probs |> 
+  gain_curve(Species, .pred_setosa:.pred_virginica) |> 
+  autoplot()
+
+iris_probs |> 
+  roc_curve(Species, .pred_setosa:.pred_virginica) |> 
+  autoplot()
+
+predict(iris_ranger, iris_testing, type = "prob") |> 
+  bind_cols(predict(iris_ranger, iris_testing)) |> 
+  bind_cols(select(iris_testing, Species)) |> 
+  glimpse()
+
+predict(iris_ranger, iris_testing, type = "prob") |> 
+  bind_cols(predict(iris_ranger, iris_testing)) |> 
+  bind_cols(select(iris_testing, Species)) |> 
+  metrics(Species, .pred_setosa:.pred_virginica, estimate = .pred_class)
